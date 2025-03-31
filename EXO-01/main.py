2436665952c417490b1bab180c373759aff5609d
yanis24sn <jackbreldb10@gmail.com>
2025-03-31 16:58:09 +0200

    Update main.py

--- EXO-01/main.py
+++ EXO-01/main.py
@@ -1,1 +1,21 @@
-print()
+import platform
+import subprocess
+
+#demander une adresse ip à l'utilisateur
+ip = input("Entez une adresse ip a ping : ")
+#on detecte l'os pour adapter la commande
+param = "-n" if platform.system().lower() == "windows" else "-c"
+#construction du ping dans un list
+commande = ["ping", param, "1", ip]
+
+print("Ping en cours")
+
+#on execute le ping
+try:
+    result = subprocess.run(commande, stdout=subprocess.DEVNULL)
+    if result.returncode == 0:
+        print("La cible est en ligne")
+    else:
+        print("Aucune reponse")
+except Exception as e:
+    print(f"Erreur lors du ping {e}")
